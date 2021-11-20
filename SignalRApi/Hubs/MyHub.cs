f316ae08f17a277f1b1ea46ca3b839d5ba581e35
Yunus <yunusozdemir468@gmail.com>
2021-11-20 20:25:50 +0300

    first commit

---
+++ SignalRApi/Hubs/MyHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApi.Hubs
+{
+    public class MyHub : Hub
+    {
+        public static List<string> Names = new List<string>();
+        public async Task SendRandomCharacter(string name)
+        {
+            Names.Add(name);
+            Random random = new Random();
+            await Clients.All.SendAsync("ReceiveMessage", random.Next(10000, 99999),name);
+        }
+        public async Task GetChar()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+        public async Task Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
+
